55a61ad5ff2642aa4970bd238bc425d2cab81ea7
Risana <risanacheerangan@gmail.com>
2026-09-19 10:13:08 +0530

    Add word frequency counter

---
+++ word_frequency.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+struct Word
+{
+    char word[50];
+    int count;
+};
+
+int main()
+{
+    char sentence[500];
+    char temp[50];
+
+    struct Word words[100];
+
+    int wordCount = 0;
+    int i = 0;
+    int j = 0;
+
+    printf("Enter a sentence:\n");
+    fgets(sentence, sizeof(sentence), stdin);
+
+    while (sentence[i] != '\0')
+    {
+        if (isalnum(sentence[i]))
+        {
+            temp[j] = tolower(sentence[i]);
+            j++;
+        }
+        else
+        {
+            if (j > 0)
+            {
+                temp[j] = '\0';
+
+                int found = 0;
+
+                for (int k = 0; k < wordCount; k++)
+                {
+                    if (strcmp(words[k].word, temp) == 0)
+                    {
+                        words[k].count++;
+                        found = 1;
+                        break;
+                    }
+                }
+
+                if (found == 0)
+                {
+                    strcpy(words[wordCount].word, temp);
+                    words[wordCount].count = 1;
+                    wordCount++;
+                }
+
+                j = 0;
+            }
+        }
+
+        i++;
+    }
+
+    /* Handle the last word */
+    if (j > 0)
+    {
+        temp[j] = '\0';
+
+        int found = 0;
+
+        for (int k = 0; k < wordCount; k++)
+        {
+            if (strcmp(words[k].word, temp) == 0)
+            {
+                words[k].count++;
+                found = 1;
+                break;
+            }
+        }
+
+        if (found == 0)
+        {
+            strcpy(words[wordCount].word, temp);
+            words[wordCount].count = 1;
+            wordCount++;
+        }
+    }
+
+    printf("\n===== Word Frequencies =====\n");
+
+    for (i = 0; i < wordCount; i++)
+    {
+        printf("%-15s %d\n", words[i].word, words[i].count);
+    }
+
+    /* Find most frequent word */
+    int max = 0;
+    int maxIndex = 0;
+
+    for (i = 0; i < wordCount; i++)
+    {
+        if (words[i].count > max)
+        {
+            max = words[i].count;
+            maxIndex = i;
+        }
+    }
+
+    printf("\nMost frequent word: %s\n", words[maxIndex].word);
+    printf("Count: %d\n", words[maxIndex].count);
+
+    return 0;
+}
